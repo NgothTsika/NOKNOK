e53103d1fbd4b854a21586fa6c485049020aca88
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-24 13:06:12 +0000

    config app

---
+++ client/app/_layout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useFonts } from "expo-font";
+import { useEffect, useState } from "react";
+import { Stack } from "expo-router";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+import "../global.css";
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    "Okra-Bold": require("../assets/fonts/Okra-Bold.ttf"),
+    "Okra-Regular": require("../assets/fonts/Okra-Regular.ttf"),
+    "Okra-Medium": require("../assets/fonts/Okra-Medium.ttf"),
+    "Okra-MediumLight": require("../assets/fonts/Okra-MediumLight.ttf"),
+    "Okra-ExtraBold": require("../assets/fonts/Okra-ExtraBold.ttf"),
+  });
+
+  const [hasNavigated, setHasNavigated] = useState(false);
+
+  useEffect(() => {
+    if (fontsLoaded && !hasNavigated) {
+      const timeoutId = setTimeout(() => {
+        setHasNavigated(true);
+      }, 1000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [fontsLoaded, hasNavigated]);
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default gestureHandlerRootHOC(Layout);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,34 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Okra-Bold": ["Okra-Bold", "sans-serif"],
+        "Okra-Medium": ["Okra-Medium", "sans-serif"],
+        "Okra-Regular": ["Okra-Regular", "sans-serif"],
+        "Okra-Mediumlight": ["Okra-MediumLight", "sans-serif"],
+        "Okra-ExtraBold": ["Okra-ExtraBold", "sans-serif"],
+      },
+      colors: {
+        primary: "#f7ca49",
+        primary_Light: "#ffe141",
+        secondary: "#0d8320",
+        text: "#363636",
+        disable: "#9197a6",
+        border: "#d0d4dc",
+        Bg_secondary: "#f5f6fb",
+        accent: {
+          100: "FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191d31",
+        },
+        danger: "#F75555",
+      },
+    },
   },
   plugins: [],
 };
