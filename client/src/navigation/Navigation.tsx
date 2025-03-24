ec15602e34416234d94bd2b4a3e4e416a4739d80
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-24 03:32:53 +0000

    config splashscreen

--- client/src/navigation/Navigation.tsx
+++ client/src/navigation/Navigation.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { FC } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Navigation = () => {
+const Stack = createNativeStackNavigator();
+
+const Navigation: FC = () => {
   return (
     <View>
       <Text>Navigation</Text>
