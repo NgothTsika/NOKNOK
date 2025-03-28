8e01f4f6f1cc8cfb8da3eb25b1c87f75d6843f1d
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-28 18:36:41 +0000

    customer Login Page

--- client/app/features/auth/CustomerLogin.tsx
+++ client/app/features/auth/CustomerLogin.tsx
@@ -1,19 +1,75 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { FC, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Animated,
+  StyleSheet,
+  Image,
+} from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  State,
+} from "react-native-gesture-handler";
+import ProductSlider from "../../../components/login/ProductSlider";
+import { resetAndNavigate } from "../../../utils/NavigationUtils";
+import Logo from "../../../assets/images/logo.png";
 
-const CustomerLogin = () => {
+const CustomerLogin: FC = () => {
+  const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+
+  const handleGesture = ({ nativeEvent }: any) => {
+    if (nativeEvent.state === State.END) {
+      const { transaltionX, transaltionY } = nativeEvent;
+      let direction = "";
+      if (Math.abs(transaltionX) > Math.abs(transaltionY)) {
+        direction = transaltionX > 0 ? "right" : "left";
+      } else {
+        direction = transaltionY > 0 ? "down" : "up";
+      }
+      console.log(transaltionX, transaltionY, direction);
+
+      const newSequence = [...gestureSequence, direction].slice(-5);
+      setGestureSequence(newSequence);
+
+      if (newSequence.join(" ") === "up up down left right") {
+        setGestureSequence([]);
+        resetAndNavigate("DeliveryLogin"); //check route after
+      }
+    }
+  };
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFFFFF",
-      }}
-    >
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Customer Login</Text>
-    </SafeAreaView>
+    <GestureHandlerRootView className="flex-1">
+      <View className="flex-1">
+        <SafeAreaView className="flex-1">
+          <ProductSlider />
+          <PanGestureHandler onHandlerStateChange={handleGesture}>
+            <Animated.ScrollView
+              contentContainerStyle={styles.subContainer}
+              bounces={false}
+              keyboardDismissMode="on-drag"
+              keyboardShouldPersistTaps="handled"
+            />
+          </PanGestureHandler>
+          {/* Place the View containing the Image at the bottom */}
+          <View className="absolute bottom-8 w-full items-center justify-center  px-5 pb-5">
+            <Image source={Logo} className="h-16 w-16 mx-3 rounded-3xl" />
+          </View>
+        </SafeAreaView>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
 export default CustomerLogin;
+
+const styles = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+});
---
+++ client/components/login/ProductSlider.tsx
@@ -0,0 +1,67 @@
+import { View, Text, Image } from "react-native";
+import React, { FC, useMemo } from "react";
+import { imageData } from "../../utils/dummyData";
+import AutoScroll from "@homielab/react-native-auto-scroll";
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+
+const ProductSlider = () => {
+  const rows = useMemo(() => {
+    const result = [];
+    for (let i = 0; i < imageData.length; i += 4) {
+      result.push(imageData.slice(i, i + 4));
+    }
+    return result;
+  }, []);
+
+  return (
+    <View pointerEvents="none">
+      <AutoScroll
+        endPaddingWidth={0}
+        duration={10000}
+        style={{ position: "absolute", zIndex: -2 }}
+      >
+        <View className="justify-center overflow-visible items-center">
+          {rows?.map((row: any, rowIndex: number) => {
+            return <MemoizedRow key={rowIndex} row={row} rowIndex={rowIndex} />;
+          })}
+        </View>
+      </AutoScroll>
+    </View>
+  );
+};
+
+const Row: FC<{ row: typeof imageData; rowIndex: number }> = ({
+  row,
+  rowIndex,
+}) => {
+  return (
+    <View className="flex-row mb-3">
+      {row.map((image, imageIndex) => {
+        const horizontalImage = rowIndex % 2 === 0 ? -18 : 18;
+        return (
+          <View
+            key={imageIndex}
+            className="mt-1 mb-3 mx-2 bg-[#ecfdf1] justify-center items-center rounded-3xl"
+            style={{
+              width: screenWidth * 0.26,
+              height: screenWidth * 0.26,
+              transform: [{ translateX: horizontalImage }],
+            }}
+          >
+            <Image
+              source={image}
+              className="w-full h-full"
+              resizeMode="contain"
+            />
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+const MemoizedRow = React.memo(Row);
+
+export default ProductSlider;
