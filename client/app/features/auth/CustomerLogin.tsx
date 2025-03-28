608e6cd5f06d2f4e0fbc7ea4396433fa00a40702
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-28 20:51:42 +0000

    Login page update

--- client/app/features/auth/CustomerLogin.tsx
+++ client/app/features/auth/CustomerLogin.tsx
@@ -15,9 +15,17 @@ import {
 import ProductSlider from "../../../components/login/ProductSlider";
 import { resetAndNavigate } from "../../../utils/NavigationUtils";
 import Logo from "../../../assets/images/logo.png";
+import CustomerText from "@/components/ui/CustomText";
+import { Fonts } from "@/utils/Constants";
+import CustomInput from "@/components/ui/CustomInput";
+import CustomButton from "@/components/ui/CustomButton";
 
 const CustomerLogin: FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [loading, setLoading] = useState(false);
   const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+
+  const handleAuth = async () => {};
 
   const handleGesture = ({ nativeEvent }: any) => {
     if (nativeEvent.state === State.END) {
@@ -56,6 +64,42 @@ const CustomerLogin: FC = () => {
           {/* Place the View containing the Image at the bottom */}
           <View className="absolute bottom-8 w-full items-center justify-center  px-5 pb-5">
             <Image source={Logo} className="h-16 w-16 mx-3 rounded-3xl" />
+            <CustomerText variants="h3" fontFamily={Fonts.Bold}>
+              Congo's last minute app{" "}
+            </CustomerText>
+            <CustomerText
+              variants="h5"
+              fontFamily={Fonts.SemiBold}
+              style={styles.text}
+            >
+              Log in or sign up
+            </CustomerText>
+
+            <CustomInput
+              onChangeText={(text) => {
+                setPhoneNumber(text.slice(0, 10));
+              }}
+              onClear={() => setPhoneNumber("")}
+              value={phoneNumber}
+              left={
+                <CustomerText
+                  style={styles.phoneText}
+                  variants="h6"
+                  fontFamily={Fonts.SemiBold}
+                >
+                  + 242
+                </CustomerText>
+              }
+              placeholder="Enter mobile number"
+              inputMode="numeric"
+            />
+
+            <CustomButton
+              title="Continue"
+              disabled={phoneNumber?.length != 10}
+              onPress={() => handleAuth()}
+              loading={loading}
+            />
           </View>
         </SafeAreaView>
       </View>
@@ -71,5 +115,13 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
     marginBottom: 20,
+  },
+  text: {
+    marginTop: 2,
+    marginBottom: 25,
+    opacity: 0.8,
+  },
+  phoneText: {
+    marginLeft: 10,
   },
 });
---
+++ client/components/ui/CustomButton.tsx
@@ -0,0 +1,57 @@
+import { Colors, Fonts } from "@/utils/Constants";
+import React, { FC } from "react";
+import { ActivityIndicator, StyleSheet, TouchableOpacity } from "react-native";
+import CustomerText from "./CustomText";
+
+interface CustomButtonProps {
+  onPress: () => void;
+  title: string;
+  disabled: boolean;
+  loading: boolean;
+}
+
+const CustomButton: FC<CustomButtonProps> = ({
+  onPress,
+  title,
+  disabled,
+  loading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      activeOpacity={0.8}
+      style={[
+        styles.btn,
+        {
+          backgroundColor: disabled ? Colors.disabled : Colors.secondary,
+        },
+      ]}
+    >
+      <ActivityIndicator color="white" size="small" />
+      <CustomerText
+        variants="h6"
+        style={styles.text}
+        fontFamily={Fonts.SemiBold}
+      >
+        {title}
+      </CustomerText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 15,
+    width: "100%",
+  },
+  text: {
+    color: "#fff",
+  },
+});
+
+export default CustomButton;
